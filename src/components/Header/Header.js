54aabf606b0367ec97b2db0072f66dcf905fa5ca
Rui Leite <leite@utfpr.edu.br>
2020-11-22 10:53:38 -0300

    Introduz conteúdo e estilo no cabeçalho.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,38 @@
+import './Header.css';
+
 function Header() {
-  return null;
+  return (
+    <nav>
+      <div className="container">
+        <a
+          href="/"
+          id="header-logo"
+        >
+          conduit
+        </a>
+        <div className="header-float-right">
+          <a
+            href="/"
+            className="header-menu"
+          >
+            Home
+          </a>
+          <a
+            href="/login"
+            className="header-menu"
+          >
+            Login
+          </a>
+          <a
+            href="/register"
+            className="header-menu"
+          >
+            Cadastrar
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
