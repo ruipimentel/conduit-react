7b53ab14b4dc94dd597b2f2cfa8c68920e79bc10
Rui Leite <leite@utfpr.edu.br>
2020-11-22 17:37:57 -0300

    Cria o formulário de autenticação, prevenindo a ação padrão.

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,8 +1,36 @@
 import './Auth.css';
 
 function Auth() {
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+  };
   return (
-    null
+    <div className="container">
+      <h1>Login</h1>
+      <div className="auth-container">
+        <form
+          onSubmit={formSubmitHandler}
+        >
+          <input
+            autoFocus
+            id="email"
+            name="email"
+            placeholder="E-mail"
+            type="text"
+          />
+          <input
+            id="password"
+            name="password"
+            type="password"
+          />
+          <button
+            type="submit"
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
