be39c23563f518a450fdc677665c02662898165d
Rui Leite <leite@utfpr.edu.br>
2020-11-22 11:21:37 -0300

    Introduz conteúdo inicial e estilo do componente `Home`.

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="center home-bg-green">
+      <h1 id="home-logo">
+        conduit
+      </h1>
+      <p id="home-lead">
+        Um lugar para você compartilhar todo o seu conhecimento!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
