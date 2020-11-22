33f2313cefc8edb8e17f04f9b84587adf4d589b3
Rui Leite <leite@utfpr.edu.br>
2020-11-22 17:57:09 -0300

    Torna o formulário de autenticação controlado.

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 import './Auth.css';
 
 function Auth() {
+
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    console.log({
+      email,
+      password
+    });
   };
   return (
     <div className="container">
@@ -13,12 +23,16 @@ function Auth() {
         >
           <input
             autoFocus
+            value={email}
+            onChange={event => setEmail(event.target.value)}
             id="email"
             name="email"
             placeholder="E-mail"
             type="text"
           />
           <input
+            value={password}
+            onChange={event => setPassword(event.target.value)}
             id="password"
             name="password"
             type="password"
