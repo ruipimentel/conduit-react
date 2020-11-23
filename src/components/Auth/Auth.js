4492ae29c52728a1f023547a68e86069e2483b58
Rui Leite <leite@utfpr.edu.br>
2020-11-23 20:06:15 -0300

    Exibe mensagens condicionais de erro no formulário de autenticação.

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -8,8 +8,13 @@ function Auth({
 }) {
 
   const [ username, setUsername ] = useState('');
+  const [ erroUsername, setErroUsername ] = useState('');
+
   const [ email, setEmail ] = useState('');
+  const [ erroEmail, setErroEmail ] = useState('');
+
   const [ password, setPassword ] = useState('');
+  const [ erroPassword, setErroPassword ] = useState('');
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
@@ -23,16 +28,22 @@ function Auth({
     onSubmit(dados);
   };
 
-  const campoUsername = <input
-    autoFocus
-    value={username}
-    onChange={event => setUsername(event.target.value)}
-    id="username"
-    name="username"
-    placeholder="Nome de usuário"
-    type="text"
-    className="col auth-input"
-  />;
+  const campoUsername = <>
+    <input
+      autoFocus
+      value={username}
+      onChange={(event) => {
+        setUsername(event.target.value);
+        setErroUsername(usernameValidator(event.target.value));
+      }}
+      id="username"
+      name="username"
+      placeholder="Nome de usuário"
+      type="text"
+      className="col auth-input"
+    />
+    <div className="auth-error">{erroUsername}</div>
+  </>;
 
   return (
     <div className="container">
@@ -41,25 +52,37 @@ function Auth({
         <form
           onSubmit={formSubmitHandler}
         >
+
           { register ? campoUsername : null }
+
           <input
             autoFocus={!register}
             value={email}
-            onChange={event => setEmail(event.target.value)}
+            onChange={(event) => {
+              setEmail(event.target.value);
+              setErroEmail(emailValidator(event.target.value));
+            }}
             id="email"
             name="email"
             placeholder="E-mail"
             type="text"
             className="col auth-input"
           />
+          <div className="auth-error">{erroEmail}</div>
+
           <input
             value={password}
-            onChange={event => setPassword(event.target.value)}
+            onChange={(event) => {
+              setPassword(event.target.value);
+              setErroPassword(passwordValidator(event.target.value));
+            }}
             id="password"
             name="password"
             type="password"
             className="col auth-input"
           />
+          <div className="auth-error">{erroPassword}</div>
+
           <div className="right">
             <button
               type="submit"
@@ -72,6 +95,46 @@ function Auth({
       </div>
     </div>
   );
+}
+
+/**
+ * Função de validação do username.
+ *
+ * Em caso de erro, retorna a mensagem a ser exibida.
+ */
+function usernameValidator(username) {
+  if (username.length === 0) {
+    return 'O nome de usuário é obrigatório.';
+  } else if (username.length > 20) {
+    return 'O nome de usuário deve possuir no máximo 20 caracteres.';
+  } else {
+    return '';
+  }
+}
+
+/**
+ * Função de validação do e-mail.
+ *
+ * Em caso de erro, retorna a mensagem a ser exibida.
+ */
+function emailValidator(email) {
+  return email.length > 0 ? '' : 'O e-mail é obrigatório.';
+}
+
+/**
+ * Função de validação da senha.
+ *
+ * Em caso de erro, retorna a mensagem a ser exibida.
+ */
+function passwordValidator(password) {
+  const contemNumero = /[0-9]/.test(password);
+  const contemMaiuscula = /[A-Z]/.test(password);
+  const contemMinuscula = /[a-z]/.test(password);
+  if (contemNumero && contemMaiuscula && contemMinuscula) {
+    return '';
+  } else {
+    return 'A senha deve conter ao menos 1 número, 1 letra maiúscula e 1 letra minúscula.';
+  }
 }
 
 export default Auth;
